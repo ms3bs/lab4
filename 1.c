ed5bdc1f9679b50397f2267058c2c15a7c51f46a
ms3b <90153608+ms3bs@users.noreply.github.com>
2021-12-24 13:02:28 +0300

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,60 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<string.h>
+//int lala(char month[])
+//{
+//	if (strcmp(month, "Feb") == 0)
+//	{
+//		return 31;
+//	}
+//	if (strcmp(month, "Mar") == 0)
+//	{
+//		return 59;
+//	}
+//	if (strcmp(month, "Apr") == 0)
+//	{
+//		return 90;
+//	}
+//	if (strcmp(month, "May") == 0)
+//	{
+//		return 120;
+//	}
+//	if (strcmp(month, "June") == 0)
+//	{
+//		return 151;
+//	}
+//	if (strcmp(month, "July") == 0)
+//	{
+//		return 181;
+//	}
+//	if (strcmp(month, "Aug") == 0)
+//	{
+//		return 212;
+//	}
+//	if (strcmp(month, "Sept") == 0)
+//	{
+//		return 243;
+//	}
+//	if (strcmp(month, "Oct") == 0)
+//	{
+//		return 273;
+//	}
+//	if (strcmp(month, "Nov") == 0)
+//	{
+//		return 304;
+//	}
+//	if (strcmp(month, "Dec") == 0)
+//	{
+//		return 334;
+//	}
+//}
+//int main()
+//{
+//	char month[4];
+//	month[0] = 'A';
+//	month[1] = 'u';
+//	month[2] = 'g';
+//	month[3] = '\0';
+//	printf("%d", lala(month));
+//	return 0;
+//}
